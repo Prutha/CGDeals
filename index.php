72aa503dc084f1ff0c28ab4bd9638abe8ba567fb
Prutha <pdesai@scu.edu>
2010-11-21 19:52:19 -0800

    Changing the header type

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php	
-header("Content-Type: application/x-blueprint+xml");    // Make sure we set the right content type
+header("Content-Type: application/x-ysnippet+xml");    // Make sure we set the right content type
 header("Cache-Control: no-cache");                      // Make sure the page isn't cached
 
 ?>
